2a4ee2b0114871610c6190d82a2ba50231cfde6b
captim <cap.tim@gmail.com>
2024-11-04 18:51:16 +0000

    init commit

---
+++ src/test/java/com/example/financehub/FinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
